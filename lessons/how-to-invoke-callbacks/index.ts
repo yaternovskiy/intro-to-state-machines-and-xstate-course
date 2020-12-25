8276b7aa6593b7e523fb5a91c61ff42f6ca4acaf
Yaroslav Ternovskiy <yaroslavternovskiy@Yaroslavs-MacBook-Pro.local>
2020-12-25 17:16:38 +0300

    send callback event

---
+++ lessons/how-to-invoke-callbacks/index.ts
@@ -0,0 +1,20 @@
+const { Machine, send } = require('xstate')
+
+const echoMachine = Machine({
+  id: 'echo',
+  initial: 'listening',
+  states: {
+    listening: {
+      on: {
+        SPEAK: {
+          actions: send('ECHO')
+        },
+        ECHO: {
+          actions: () => {
+            console.log('echo, echo')
+          },
+        },
+      },
+    },
+  },
+})
